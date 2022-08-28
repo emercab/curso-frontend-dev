f6eef1c0314979a2816f3d9a47adc2e0449d8527
Emerson Cabrera Salas <emercab@gmail.com>
2022-08-28 15:55:22 -0500

    Aside shopping car added

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const btnMobileMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const btnShoppingCar = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleSubMenuEmail);
 btnMobileMenu.addEventListener('click', showMobileMenu);
+btnShoppingCar.addEventListener('click', toggleAside);
 
 function toggleSubMenuEmail() {
+   aside.classList.remove('show-aside')
    desktopMenu.classList.toggle('inactive');
 }
 
 function showMobileMenu() {
+   aside.classList.remove('show-aside')
    mobileMenu.classList.toggle('show-mobile-menu')
+}
+
+function toggleAside() {
+   desktopMenu.classList.add('inactive');
+   mobileMenu.classList.remove('show-mobile-menu');
+   aside.classList.toggle('show-aside')
 }
