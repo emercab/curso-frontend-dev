9db5e795e62c7ef780ff5169aeb869f31fb2d17f
Emerson Cabrera Salas <emercab@gmail.com>
2022-08-28 11:17:02 -0500

    Submenu Mobile  added

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 // Toggle submenu email
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnMobileMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleSubMenuEmail);
+btnMobileMenu.addEventListener('click', showMobileMenu);
 
 function toggleSubMenuEmail() {
    desktopMenu.classList.toggle('inactive');
+}
+
+function showMobileMenu() {
+   mobileMenu.classList.toggle('show-mobile-menu')
 }
